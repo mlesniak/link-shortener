43bb5408a8cc6928cce2d74e3457096b5230267c
Michael Lesniak <mail@mlesniak.com>
2024-02-16 20:26:50 +0100

    Initial commit

---
+++ src/test/java/com/mlesniak/shortener/LinkShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mlesniak.shortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkShortenerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
