8e62d39cec16d401a3d0eb70df7e48e4b458a91f
Michael Lesniak <mail@mlesniak.com>
2024-02-16 20:48:55 +0100

    Add persistence

--- src/main/java/com/mlesniak/shortener/LinkController.java
+++ src/main/java/com/mlesniak/shortener/LinkController.java
@@ -5,8 +5,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class LinkController {
+    private Map<String, String> links = new HashMap<>();
+
     public record CreateLinkRequest(String url) {
 
     }
@@ -15,18 +22,40 @@ public class LinkController {
 
     }
 
-
     @PostMapping("/api/link")
     public CreateLinkResponse add(@RequestBody CreateLinkRequest request) {
-        String id = "12345";
+        var sha = getSHA256(request.url);
+        links.put(sha, request.url);
+        var id = sha;
         return new CreateLinkResponse(id, request.url, request.url);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Void> get(@PathVariable String id) {
+        var url = links.get(id);
+        if (url == null) {
+            return ResponseEntity.notFound().build();
+        }
+
         return ResponseEntity
                 .status(HttpStatus.FOUND)
-                .header(HttpHeaders.LOCATION, id)
+                .header(HttpHeaders.LOCATION, url)
                 .build();
+    }
+
+    private String getSHA256(String input) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] hash = md.digest(input.getBytes(StandardCharsets.UTF_8));
+            StringBuilder hexString = new StringBuilder();
+            for (byte b : hash) {
+                String hex = Integer.toHexString(0xff & b);
+                if(hex.length() == 1) hexString.append('0');
+                hexString.append(hex);
+            }
+            return hexString.toString();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
